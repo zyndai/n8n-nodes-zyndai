ea8d19599e43c8f4582cf9f817a668ce414b8bf2
Swapnil Shinde <swapnilshinde9382@gmail.com>
2026-02-08 15:34:09 +0530

    fix zynd icon not showing in the nodes

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,16 @@
 const { src, dest, parallel } = require('gulp');
 
-function copyIcons() {
+function copyNodeIcons() {
     return src('nodes/**/*.svg').pipe(dest('dist/nodes'));
+}
+
+function copyRootIcons() {
+    return src('icons/**/*.{svg,png}').pipe(dest('dist/icons'));
 }
 
 function copyJson() {
     return src('nodes/**/*.json').pipe(dest('dist/nodes'));
 }
 
-exports.default = parallel(copyIcons, copyJson);
-exports.build = parallel(copyIcons, copyJson);
+exports.default = parallel(copyNodeIcons, copyRootIcons, copyJson);
+exports.build = parallel(copyNodeIcons, copyRootIcons, copyJson);
